a2bffbf36c7fd09d6d9446576ec1a71cd98bcc7c
Lorenzo S <46894435+Suffro@users.noreply.github.com>
2022-07-10 22:52:54 +0200

    Automatic commit from Bubble

--- actions/AAC-850m6/server.js
+++ actions/AAC-850m6/server.js
@@ -1,18 +1,18 @@
 function(properties, context) {
     
-    let json_result = {};
-    let pairs_array = properties.pairs;
-    let obj={};
-	
-    for (const pair of pairs_array) {
-  		obj[pair.key]=pair.value;
-	}
+    let thing = properties.thing;
+    let props = thing.listProperties();
+    let obj={}
+	for(let i=0; i<props.length; i++){
+        if(typeof thing.get(props[i]) == "string"){
+        	obj[props[i]] = thing.get(props[i]);
+        }
+    }
     
-    let stringified_obj = JSON.stringify(obj);
-    
-	return(
+    let returnJson = JSON.stringify(obj);
+    return (
     	{
-            json: stringified_obj
+            json: returnJson
         }
     );
 
